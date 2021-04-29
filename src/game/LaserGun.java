59f1ca0fb83b9734c09cdc54d0bd08add9f165b9
TIMJORDANLAPTOP\jorda <jordantim0918@gmail.com>
2021-04-30 09:54:06 +1000

    created LaserGun class with specialised constructor

---
+++ src/game/LaserGun.java
@@ -0,0 +1,12 @@
+package game;
+
+import edu.monash.fit2099.engine.WeaponItem;
+
+public class LaserGun extends WeaponItem {
+    /**
+     * Constructor.
+     */
+    public LaserGun() {
+        super("laser", '>', 50, "zaps");
+    }
+}
