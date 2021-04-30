187812153328414829efb28df2e94b41b1de9e73
TIMJORDANLAPTOP\jorda <jordantim0918@gmail.com>
2021-04-30 11:55:39 +1000

    added ecopoints class. points always start at 0 and points can be added later on

---
+++ src/game/Ecopoints.java
@@ -0,0 +1,17 @@
+package game;
+
+public class Ecopoints {
+    private int points;
+
+    public Ecopoints() {
+        this.points = 0;
+    }
+
+    public void addPoints(int points) {
+        this.points = points;
+    }
+
+    public int getPoints() {
+        return this.points;
+    }
+}
