156599a38f357829621f22affd9f04b0de46fad1
TIMJORDANLAPTOP\jorda <jordantim0918@gmail.com>
2021-05-01 20:35:59 +1000

    created dinosaur egg base abstract class. contains tick function that will delete egg and create instance of a dinosaur

---
+++ src/game/eggs/DinosaurEgg.java
@@ -0,0 +1,35 @@
+package game.eggs;
+
+import edu.monash.fit2099.engine.Action;
+import edu.monash.fit2099.engine.Location;
+import game.Counter;
+import game.PortableItem;
+import game.dinosaur.Dinosaur;
+
+import java.util.List;
+
+public abstract class DinosaurEgg extends PortableItem {
+    Counter lifespan;
+    public DinosaurEgg(String name, int lifespan) {
+        super(name, 'e');
+        this.lifespan = new Counter(lifespan);
+    }
+
+    @Override
+    public List<Action> getAllowableActions() {
+        return super.getAllowableActions();
+    }
+
+    @Override
+    public void tick(Location currentLocation) {
+        super.tick(currentLocation);
+        lifespan.dec();
+        if (lifespan.getValue() == 0) {
+            currentLocation.removeItem(this);
+        }
+        Dinosaur dinosaur = createDinosaur();
+        currentLocation.addActor(dinosaur);
+    }
+
+    abstract Dinosaur createDinosaur();
+}
