75c99844fc8b06b1e9f53f6f2ddc894620139bbd
TIMJORDANLAPTOP\jorda <jordantim0918@gmail.com>
2021-05-01 19:46:18 +1000

    overrided tick function when on ground to decrement lifespan and delete remove item from location when lifespan is 0

--- src/game/Corpse.java
+++ src/game/Corpse.java
@@ -1,9 +1,26 @@
 package game;
 
-import game.PortableItems.PortableItem;
+import edu.monash.fit2099.engine.Location;
 
 public class Corpse extends PortableItem {
-    public Corpse(String name, char displayChar) {
+
+    /**
+     * A counter class that represents the amount of turns left until item disappears
+     */
+    private Counter lifespan;
+
+    public Corpse(String name) {
         super("dead " + name, '%');
+        lifespan = new Counter(20);
+    }
+
+    @Override
+    public void tick(Location currentLocation) {
+        super.tick(currentLocation);
+        lifespan.dec();
+        if (lifespan.getValue() == 0) {
+            currentLocation.removeItem(this);
+        }
+
     }
 }
