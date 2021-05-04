bc50709cfceaee6f08e9ddef30c21cd59c1c1b17
enoki123 <eleo0010@student.monash.edu>
2021-05-04 21:46:00 +1000

    Amended Dinosaur methods to fit new parameters

--- src/game/dinosaur/Dinosaur.java
+++ src/game/dinosaur/Dinosaur.java
@@ -8,19 +8,21 @@ import edu.monash.fit2099.engine.DoNothingAction;
 import edu.monash.fit2099.engine.GameMap;
 
 import game.Behaviour;
+import game.Counter;
 import game.WanderBehaviour;
 
+import java.awt.*;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public abstract class Dinosaur extends Actor {
     protected ArrayList<Behaviour> behaviour = new ArrayList<Behaviour>();
     protected int hungryhealth;
     protected int breedinghealth;
-
-
-
+    protected HashMap<Dinosaur, Counter> dinosaurAttackers;
     /**
-     * Constructor for Dinosaur Class.
+     * Constructor.
      *
      * @param name        the name of the Actor
      * @param displayChar the character that will represent the Actor in the display
@@ -29,12 +31,24 @@ public abstract class Dinosaur extends Actor {
     public Dinosaur(String name, char displayChar, int hitPoints) {
         super(name, displayChar, hitPoints);
         behaviour.add(new WanderBehaviour());
-
+        dinosaurAttackers = new HashMap<>();
     }
+
+
 
     @Override
     public Action playTurn(Actions actions, Action lastAction, GameMap map, Display display) {
         this.hurt(1);
+
+        for (Dinosaur dinosaur : dinosaurAttackers.keySet()) {
+            Counter attackTimer = dinosaurAttackers.get(dinosaur);
+            attackTimer.dec();
+            if (attackTimer.getValue() == 0) {
+                dinosaurAttackers.remove(dinosaur);
+            }
+
+        }
+
         for (Behaviour thisbehaviour : behaviour) {
             Action action = thisbehaviour.getAction(this, map);
             if (action != null)
@@ -42,6 +56,9 @@ public abstract class Dinosaur extends Actor {
         }
         return new DoNothingAction();
     }
+
+    public void addAttacker(Dinosaur dinosaur) {dinosaurAttackers.put(dinosaur, createTimeoutCounter());
+    }
+
+    abstract Counter createTimeoutCounter();
 }
-
-
