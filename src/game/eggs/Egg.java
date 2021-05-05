960121cbea835d7f7329f0ad4cbcd92f0b7642cf
TIMJORDANLAPTOP\jorda <jordantim0918@gmail.com>
2021-05-05 12:41:25 +1000

    added player ecopoints as an egg attribute. created increaseEcoPoints abstract class that increments player's ecopoints

--- src/game/eggs/Egg.java
+++ src/game/eggs/Egg.java
@@ -2,6 +2,7 @@ package game.eggs;
 
 import edu.monash.fit2099.engine.Location;
 import game.Counter;
+import game.Ecopoints;
 import game.PortableItem;
 import game.actions.EatMeatAction;
 import game.dinosaur.Dinosaur;
@@ -10,10 +11,12 @@ import game.enums.DietCapabilities;
 
 public abstract class Egg extends PortableItem {
     Counter lifespan;
+    Ecopoints ecopoints;
 
-    public Egg(String name, int lifespan) {
+    public Egg(String name, int lifespan, Ecopoints ecopoints) {
         super(name, 'e');
         this.lifespan = new Counter(lifespan);
+        this.ecopoints = ecopoints;
         addCapability(DietCapabilities.MEAT);
         allowableActions.add(new EatMeatAction(this));
     }
@@ -23,6 +26,7 @@ public abstract class Egg extends PortableItem {
         super.tick(currentLocation);
         lifespan.dec();
         if (lifespan.getValue() == 0) {
+            increaseEcoPoints();
             currentLocation.removeItem(this);
         }
         Dinosaur dinosaur = createDinosaur();
@@ -30,4 +34,6 @@ public abstract class Egg extends PortableItem {
     }
 
     abstract Dinosaur createDinosaur();
+
+    abstract void increaseEcoPoints();
 }
