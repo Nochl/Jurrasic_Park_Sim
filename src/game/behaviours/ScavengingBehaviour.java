6ba03a7c0ad0c5700f1ae4fd10010348a4c7407f
TIMJORDANLAPTOP\jorda <jordantim0918@gmail.com>
2021-05-07 19:29:11 +1000

    Added Javadocs

--- src/game/behaviours/ScavengingBehaviour.java
+++ src/game/behaviours/ScavengingBehaviour.java
@@ -8,6 +8,19 @@ import game.enums.FoodTypeCapabilities;
 
 import java.util.ArrayList;
 
+/**
+ * Implements Scavenging Behaviour class that implements Behaviour interface.
+ * Utilised by the Dinosaur class to find and get the closest item food source
+ * within the game world
+ * @author Tim Jordan
+ * @author Enoch Leow
+ * @version 1.0.0
+ * @see game.dinosaur.Dinosaur
+ * @see FoodTypeCapabilities
+ * @see FindNearestLocation
+ * @see EatFruitAction
+ * @see Behaviour
+ */
 public class ScavengingBehaviour implements Behaviour {
     LocationFollowBehaviour followTarget = null;
 
@@ -39,6 +52,11 @@ public class ScavengingBehaviour implements Behaviour {
         return followTarget.getAction(actor, actions, map);
     }
 
+    /**
+     * Checks if the given location contains items that have VEGETABLE Food Type capability
+     * @param location A Location class object
+     * @return a boolean True if the location contains a VEGETABLE item, else False
+     */
     private boolean containsSuitableItem(Location location) {
         for (Item item : location.getItems()) {
             if (item.hasCapability(FoodTypeCapabilities.VEGETABLE)) {
