9a8792d74a7065c8c84136740c2e70a961978c0a
TIMJORDANLAPTOP\jorda <jordantim0918@gmail.com>
2021-05-12 13:52:49 +1000

    Made it so the actors are not able to enters and blocks thrown objects

--- src/game/VendingMachine.java
+++ src/game/VendingMachine.java
@@ -3,7 +3,9 @@ package game;
 import edu.monash.fit2099.engine.*;
 import game.actions.VendingAction;
 
-
+/**
+ * Implements a Vending Machine class object that extends from Ground
+ */
 public class VendingMachine extends Ground {
 
     /***
@@ -13,6 +15,27 @@ public class VendingMachine extends Ground {
         super('!');
     }
 
+    @Override
+    public boolean canActorEnter(Actor actor) {
+        return false;
+    }
+
+    /**
+     * Automatically blocks thrown objects at Vending Machine
+     * @return a boolean true
+     */
+    @Override
+    public boolean blocksThrownObjects() {
+        return true;
+    }
+
+    /**
+     * Returns allowable actions that can be done to Vending Machine
+     * @param actor the Actor acting
+     * @param location the current Location
+     * @param direction the direction of the Ground from the Actor
+     * @return an Actions class containing actions that can be done to Vending Machine
+     */
     @Override
     public Actions allowableActions(Actor actor, Location location, String direction) {
         Actions actions = new Actions();
