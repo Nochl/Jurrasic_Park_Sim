da61f89ab4cb25706ae2be6ea86fe73976b66ce8
TIMJORDANLAPTOP\jorda <jordantim0918@gmail.com>
2021-05-12 13:54:09 +1000

    Added javadocs and changed the instance creation of vendingItemFactory

--- src/game/actions/VendingAction.java
+++ src/game/actions/VendingAction.java
@@ -22,8 +22,11 @@ public class VendingAction extends Action {
     private VendingItemFactory vendingItemFactory;
     private ArrayList<VendingMachineItems> menuOptions;
 
+    /**
+     * Constructor for Vending Action
+     */
     public VendingAction() {
-        vendingItemFactory = new VendingItemFactory();
+        vendingItemFactory = VendingItemFactory.getInstance();
         ArrayList<VendingMachineItems> vendingMachineOptions = vendingItemFactory.getPossibleVendingItems();
         // Null object is added so indices and options align
         menuOptions = new ArrayList<>();
@@ -33,6 +36,12 @@ public class VendingAction extends Action {
         }
     }
 
+    /**
+     * Allows the player actor to purchase a vending machine item
+     * @param actor The actor performing the action.
+     * @param map The map the actor is on.
+     * @return a string describing the player's actions
+     */
     @Override
     public String execute(Actor actor, GameMap map) {
         if (!(actor instanceof Player)) {
@@ -40,7 +49,7 @@ public class VendingAction extends Action {
         }
         Display display = new Display();
         displayVendingMachineMenu((Player)actor, display);
-        int key = getActorMenuOption(actor, display);
+        int key = getActorMenuOption(display);
         String description;
         Ecopoints playerEcopoints = ((Player)actor).getEcopoints();
         int itemCost = menuOptions.get(key).getCost();
@@ -60,19 +69,18 @@ public class VendingAction extends Action {
      * Processes the player's input and checks if they chosen a valid number
      * and that they have enough ecopoints.
      *
-     * @param actor
-     * @param display
-     * @return
+     * @param display a Display object that enable I/O operations
+     * @return a int value that represents a menu option
      */
-    private int getActorMenuOption(Actor actor, Display display) {
+    private int getActorMenuOption(Display display) {
         char key;
         boolean validKey = false;
         do {
             key = display.readChar();
-            if (key > 0 && key <= menuOptions.size()) {
+            if (key > 0 && key <= menuOptions.size() && Character.isDigit(key)) {
                 validKey = true;
             }
-        } while (!Character.isDigit(key) && validKey);
+        } while (!validKey);
         return Character.getNumericValue(key);
     }
 
@@ -95,11 +103,20 @@ public class VendingAction extends Action {
         System.out.println("EcoPoints : " + player.getEcopoints().getPoints());
     }
 
+    /**
+     * Provides a description of action accessing vending machine in game world
+     * @param actor The actor performing the action.
+     * @return a string containing the action the player can do
+     */
     @Override
     public String menuDescription(Actor actor) {
         return "access vending machine";
     }
 
+    /**
+     * sets the specific hotkey to run this action as 'a'
+     * @return a string of the hotkey
+     */
     @Override
     public String hotkey() {
         return "a";
