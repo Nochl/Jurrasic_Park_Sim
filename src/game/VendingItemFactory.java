0aa97657d6750807b39c27af0a26142c16def260
TIMJORDANLAPTOP\jorda <jordantim0918@gmail.com>
2021-05-14 11:26:44 +1000

    Added Javadocs to class and methods

--- src/game/VendingItemFactory.java
+++ src/game/VendingItemFactory.java
@@ -19,8 +19,15 @@ import java.util.ArrayList;
  * @version 3.0.0
  */
 public class VendingItemFactory {
+    /**
+     * a static attribute of VendingItemFactory
+     */
     static VendingItemFactory factory = null;
 
+    /**
+     * A public method that returns a singleton of VendingItemFactory
+     * @return a reference to VendingItemFactory singleton
+     */
     public static VendingItemFactory getInstance(){
         if (factory == null) {
             factory = new VendingItemFactory();
@@ -28,9 +35,16 @@ public class VendingItemFactory {
         return factory;
     }
 
+    /**
+     * private constructor for VendingItemFactory
+     */
     private VendingItemFactory() {
     }
 
+    /**
+     * returns an ArrayList of possible Vending Machine Items and corresponding costs
+     * @return an ArrayList containing all VendingMachineItems values and enumerators
+     */
     public ArrayList<VendingMachineItems> getPossibleVendingItems() {
         ArrayList<VendingMachineItems> possibleVendingItems = new ArrayList<>();
         for (VendingMachineItems item : VendingMachineItems.values()) {
@@ -39,6 +53,12 @@ public class VendingItemFactory {
         return possibleVendingItems;
     }
 
+    /**
+     * Creates an instance of a item depending on the given VendingMachineItems
+     * input
+     * @param vendingMachineItem an instance of VendingMachineItems that details a vending machine items
+     * @return an instance of Item that is a vending machine item
+     */
     public Item createVendingItem(VendingMachineItems vendingMachineItem) {
         Item item = switch (vendingMachineItem) {
             case FRUIT -> createFruit();
@@ -48,35 +68,62 @@ public class VendingItemFactory {
             case BRACHIOSAUR_EGG -> createBrachiosaurEgg();
             case ALLOSAUR_EGG -> createAllosaurEgg();
             case LASER_GUN -> createLaserGun();
-            default -> null;
         };
         return item;
     }
 
+    /**
+     * Creates an instance of Item fruit
+     * @return a fruit Item
+     */
     private Item createFruit() {
         return new Fruit();
     }
 
+    /**
+     * Creates an instance of Item vegetarian meal kit
+     * @return a vegetarian meal kit Item
+     */
     private Item createVegetarianMealKit() {
         return new VegetarianMealKit();
     }
 
+    /**
+     * Creates an instance of Item carnivore meal kit
+     * @return a carnivore meal kit Item
+     */
     private Item createCarnivoreMealKit() {
         return new CarnivoreMealKit();
     }
 
+    /**
+     * Creates an instance of Item laser gun
+     * @return a laser gun Item
+     */
     private Item createLaserGun() {
         return new LaserGun();
     }
 
+    /**
+     * Creates an instance of Item allosaur egg
+     * @return an allosaur egg Item
+     */
     private Item createAllosaurEgg() {
         return new AllosaurEgg();
     }
 
+    /**
+     * Creates an instance of Item brachiosaur egg
+     * @return a brachiosaur egg Item
+     */
     private Item createBrachiosaurEgg() {
         return new BrachiosaurEgg();
     }
 
+    /**
+     * Creates an instance of Item stegosaur egg
+     * @return a stegosaur egg Item
+     */
     private Item createStegosaurEgg() {
         return new StegosaurEgg();
     }
