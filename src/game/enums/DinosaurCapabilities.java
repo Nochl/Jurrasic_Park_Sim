2d33ce1a8560958303b75fa4370382b59ed2104f
TIMJORDANLAPTOP\jorda <jordantim0918@gmail.com>
2021-05-20 17:40:13 +1000

    Added Pterodactyl as a dinosaur type

--- src/game/enums/DinosaurCapabilities.java
+++ src/game/enums/DinosaurCapabilities.java
@@ -1,5 +1,11 @@
 package game.enums;
 
+/**
+ * Implements an enumerator class of all dinosaur types
+ * @author Tim Jordan
+ * @author Enoch Leow
+ * @version 1.0.0
+ */
 public enum DinosaurCapabilities {
-    STEGOSAUR, BRACHIOSAUR, ALLOSAUR
+    STEGOSAUR, BRACHIOSAUR, ALLOSAUR, PTERODACTYL
 }
