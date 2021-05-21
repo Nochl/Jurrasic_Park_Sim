cf5ee16df426ed65d7c76ca59c405cd3e69d3761
TIMJORDANLAPTOP\jorda <jordantim0918@gmail.com>
2021-05-21 16:16:29 +1000

    Added Javadocs to class an methods. Also added a reset counter method

--- src/game/Counter.java
+++ src/game/Counter.java
@@ -1,20 +1,49 @@
 package game;
 
+/**
+ * Implements a counter class
+ * @author Tim Jordan
+ * @author Enoch Leow
+ * @version 1.0.1
+ */
 public class Counter {
+    /**
+     * an int containing the maximum value counter can be
+     */
     private int max;
+    /**
+     * an int containing the current value of counter
+     */
     private int value;
 
+    /**
+     * Constructor
+     * @param max an int denoting the max value of counter
+     */
     public Counter(int max) {
         this.max = max;
         value = max;
     }
 
+    /**
+     * Decrements the counter's current value by 1
+     */
     public void dec() {
         value -= 1;
     }
 
+    /**
+     * Gets the value of counter
+     * @return an int denoting the value of counter
+     */
     public int getValue() {
         return value;
     }
 
+    /**
+     * Resets the counter to its max possible value
+     */
+    public void resetCounter() {
+        value = max;
+    }
 }
