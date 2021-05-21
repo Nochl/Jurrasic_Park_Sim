2e8a9405661633b45beb0bb4a8145cac5fbcf80d
TIMJORDANLAPTOP\jorda <jordantim0918@gmail.com>
2021-05-21 11:25:33 +1000

    Created an enum class that distinguishes different types of mobility for actors

---
+++ src/game/enums/ActorMobilityCapabilities.java
@@ -0,0 +1,11 @@
+package game.enums;
+
+/**
+ * Implements an enumerator class that distinguishes different types of mobility for actors
+ * @author Tim Jordan
+ * @author Enoch Leow
+ * @version 1.0.0
+ */
+public enum ActorMobilityCapabilities {
+    WALK, FLY,
+}
