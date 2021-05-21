4a99f8b3c23271dafdd1b549b95ec9a0ad3c2eea
TIMJORDANLAPTOP\jorda <jordantim0918@gmail.com>
2021-05-21 11:23:02 +1000

    Initialised Lake Ground class. Currently not able to enable actors to drink from Lake.java

---
+++ src/game/ground/Lake.java
@@ -0,0 +1,70 @@
+package game.ground;
+
+import edu.monash.fit2099.engine.Actions;
+import edu.monash.fit2099.engine.Actor;
+import edu.monash.fit2099.engine.Ground;
+import edu.monash.fit2099.engine.Location;
+import game.consumable.Fish;
+import game.enums.ActorMobilityCapabilities;
+import game.utils.RandomNumberGenerator;
+
+import java.util.ArrayList;
+
+/**
+ * Implements a lake ground object in game world
+ *
+ * @author Tim Jordan
+ * @author Enoch Leow
+ * @version 1.0.0
+ * @see Ground
+ */
+public class Lake extends Ground {
+
+    /**
+     *  an int denoting the amount of sips an actor can take from lake
+     */
+    int capacity;
+
+    /**
+     * An array list of fish in the lake
+     */
+    ArrayList<Fish> seaCreatures;
+
+    /**
+     * Ini
+     */
+    int initialFishAmount;
+    /**
+     * Constructor.
+     *
+     * @param displayChar character to display for this type of terrain
+     */
+    public Lake(char displayChar) {
+        super('~');
+        initialFishAmount = 5;
+        seaCreatures = new ArrayList<>();
+        for (int i = 0; i < initialFishAmount; i++) {
+            seaCreatures.add(new Fish());
+        }
+    }
+
+    @Override
+    public Actions allowableActions(Actor actor, Location location, String direction) {
+//        return new DrinkLakeAction();
+        return null;
+    }
+
+    @Override
+    public void tick(Location location) {
+        super.tick(location);
+        Double randomNumber = RandomNumberGenerator.randomDouble();
+        if (randomNumber <= 0.6) {
+            seaCreatures.add(new Fish());
+        }
+    }
+
+    @Override
+    public boolean canActorEnter(Actor actor) {
+        return actor.hasCapability(ActorMobilityCapabilities.FLY);
+    }
+}
