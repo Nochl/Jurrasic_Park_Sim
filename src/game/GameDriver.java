dc4e51d07bcb0ff09df043676d44f8b6fffe564c
TIMJORDANLAPTOP\jorda <jordantim0918@gmail.com>
2021-05-25 18:41:52 +1000

    Added InitialiseDinosaur object and modified StartGame function to use new method and initialise more dinosaurs

--- src/game/GameDriver.java
+++ src/game/GameDriver.java
@@ -1,12 +1,14 @@
 package game;
 
 import edu.monash.fit2099.engine.*;
+import game.dinosaur.Brachiosaur;
 import game.dinosaur.Dinosaur;
 import game.dinosaur.Pterodactyl;
 import game.dinosaur.Stegosaur;
 import game.enums.GameModes;
 import game.ground.*;
 import game.utils.GetUserInput;
+import game.utils.RandomNumberGenerator;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -83,25 +85,25 @@ public class GameDriver {
             List<String> map2 = Arrays.asList(
                     "................................................................................",
                     "................................................................................",
+                    ".........++.....................................................................",
+                    "..........+...........................................................++........",
+                    "....................................................................++++........",
+                    "................................................................................",
+                    "................................~~~~~~~~~~~~....................................",
+                    ".................+++........~~~~~~~~~~~~~~~~~~~.................................",
+                    ".................+++.....~~~~~~~~~~~~~~~~~~~~~~~~~..............................",
+                    ".......................~~~~~~~~~~~~~~~~~~~~~~~~~~~~.............................",
+                    ".......................~~~~~~~~~~~~~~~~~~~~~~~~~~~~.............................",
+                    ".........................~~~~~~~~~~~~~~~~~~~~~~~~~..............................",
+                    "............................~~~~~~~~~~~~~~~~~~~.................................",
+                    "........+.......................~~~~~~~~~~~~....................................",
+                    "........++......................................................................",
                     "................................................................................",
                     "................................................................................",
                     "................................................................................",
-                    "................................................................................",
-                    "................................................................................",
-                    "................................................................................",
-                    "................................................................................",
-                    "................................................................................",
-                    "................................................................................",
-                    "................................................................................",
-                    "................................................................................",
-                    "................................................................................",
-                    "................................................................................",
-                    "................................................................................",
-                    "................................................................................",
-                    "................................................................................",
-                    "................................................................................",
-                    "................................................................................",
-                    "................................................................................",
+                    "................+..................................................++...........",
+                    "...............+.+................................................++++++........",
+                    "................+...................................................++++........",
                     "................................................................................",
                     "................................................................................",
                     "................................................................................");
@@ -117,16 +119,37 @@ public class GameDriver {
             MapSwitch.addSwitch(gameMap, "top");
             MapSwitch.addSwitch(gameMap2, "bottom");
 
-            // Place a pair of stegosaurs in the middle of the map
-            Dinosaur dinosaur1 = new Stegosaur("Stegosaur", false, 'm');
-            Dinosaur dinosaur2 = new Stegosaur("Stegosaur",false, 'f');
-            Dinosaur dinosaur3 = new Pterodactyl("Pterodactyl",false, 'm');
-            gameMap.at(30, 12).addActor(dinosaur1);
-            gameMap.at(32, 12).addActor(dinosaur2);
-            gameMap.at(10, 10).addActor(dinosaur3);
-            DinosaurHold.addDinosaur(dinosaur1);
-            DinosaurHold.addDinosaur(dinosaur2);
-            DinosaurHold.addDinosaur(dinosaur3);
+            // Initialise Dinosaurs
+            Dinosaur stegosaur1 = new Stegosaur("Stegosaur", false, 'm');
+            Dinosaur stegosaur2 = new Stegosaur("Stegosaur",false, 'f');
+            Dinosaur stegosaur3 = new Stegosaur("Stegosaur",false, 'f');
+            Dinosaur stegosaur4 = new Stegosaur("Stegosaur",false, 'm');
+            Dinosaur pterodactyl1 = new Pterodactyl("Pterodactyl",false, 'm');
+            Dinosaur pterodactyl2 = new Pterodactyl("Pterodactyl",false, 'f');
+            Dinosaur pterodactyl3 = new Pterodactyl("Pterodactyl",false, 'm');
+            Dinosaur pterodactyl4 = new Pterodactyl("Pterodactyl",false, 'f');
+            Dinosaur brachiosaur1 = new Brachiosaur("Brachiosaur", false, 'm');
+            Dinosaur brachiosaur2 = new Brachiosaur("Brachiosaur", false, 'f');
+            Dinosaur brachiosaur3 = new Brachiosaur("Brachiosaur", false, 'm');
+            Dinosaur brachiosaur4 = new Brachiosaur("Brachiosaur", false, 'f');
+
+            // Add Dinosaurs to game map
+            initialiseDinosaur(gameMap, 30, 12, stegosaur1);
+            initialiseDinosaur(gameMap, 32, 12, stegosaur2);
+            initialiseDinosaur(gameMap, 10, 10, brachiosaur1);
+            initialiseDinosaur(gameMap, 11, 11, brachiosaur2);
+            initialiseDinosaur(gameMap, 20, 20, pterodactyl3);
+            initialiseDinosaur(gameMap, 20, 21, pterodactyl4);
+
+            // Add Dinosaur to game map
+            initialiseDinosaur(gameMap2, 32, 20, pterodactyl1);
+            initialiseDinosaur(gameMap2, 30, 20, pterodactyl2);
+            initialiseDinosaur(gameMap2, 12, 10, stegosaur3);
+            initialiseDinosaur(gameMap2, 12, 12, stegosaur4);
+            initialiseDinosaur(gameMap2, 21, 15, brachiosaur3);
+            initialiseDinosaur(gameMap2, 20, 15, brachiosaur4);
+
+
 
             showGameMenu(display);
             setGameMode(display, player);
@@ -142,6 +165,20 @@ public class GameDriver {
             }
         } while (continueGame);
 
+    }
+
+    /**
+     * Adds dinosaur into game map at location x and y and adds it into DinosaurHold
+     * @param map a GameMap object
+     * @param x an x coordinate in gamemap
+     * @param y a y coordinate in game map
+     * @param dinosaur a Dinosaur Object
+     */
+    private void initialiseDinosaur(GameMap map, int x, int y, Dinosaur dinosaur) {
+        DinosaurHold.addDinosaur(dinosaur);
+        if (!map.at(x, y).containsAnActor()) {
+            map.at(x, y).addActor(dinosaur);
+        }
     }
 
     /**
@@ -189,4 +226,5 @@ public class GameDriver {
             display.println(" " + i + ") " + gameModeName);
         }
     }
+    
 }
