e50f8337a98786ce90b35057ef1ae1fbee51e064
TIMJORDANLAPTOP\jorda <jordantim0918@gmail.com>
2021-05-25 21:53:50 +1000

    Added javadocs to class and class methods

--- src/game/consumable/corpse/PterodactylCorpse.java
+++ src/game/consumable/corpse/PterodactylCorpse.java
@@ -2,6 +2,14 @@ package game.consumable.corpse;
 
 import edu.monash.fit2099.engine.Actor;
 
+/**
+ * Implements the corpse of a Pterodactyl
+ * @author Tim Jordan
+ * @author Enoch Leow
+ * @version 1.0.0
+ * @see game.dinosaur.Pterodactyl
+ * @see Corpse
+ */
 public class PterodactylCorpse extends Corpse {
     /**
      * Constructor for Corpse class
